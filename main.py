54ae4d930bd65f315941ed10d9891bd554fb15d1
Muhammad Abdullah <232841637+abdullahee297@users.noreply.github.com>
2026-01-02 11:39:09 +0500

    Initialization of webcam

--- main.py
+++ main.py
@@ -1,4 +1,33 @@
 import cv2
 import time
 import mediapipe as mp 
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
 
+BaseOptions = python.BaseOptions
+HandLandmarker = vision.HandLandmarker
+HandLandmarkerOptions = vision.HandLandmarkerOptions
+VisionRunningMode = vision.RunningMode
+
+options = HandLandmarkerOptions(
+    base_options = BaseOptions(model_asset_path = "hand_landmarker.task"),
+    running_mode = VisionRunningMode.IMAGE,
+    num_hands = 2
+)
+
+detector = HandLandmarker.create_from_options(options)
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    success, img = cap.read()
+    
+    if not success:
+        break
+
+    cv2.imshow("Volume Control ML Model", img)
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows
